dda6d062021fec97cf183249e251d0c338618331
JohanCotrina <johancotrina1233@gmail.com>
2026-06-21 14:59:25 -0500

    agregar documentación Swagger UI y datos de prueba iniciales

---
+++ api-pizzeria/api-pizzeria/src/main/java/com/pizzeria/api_pizzeria/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.pizzeria.api_pizzeria.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Pizzería")
+                        .description("Documentación centralizada de la API para el sistema de gestión de la Pizzería")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Desarrollo Backend Pizzería")
+                                .email("dev@pizzeria.com"))
+                        .license(new License()
+                                .name("Licencia Interna")
+                                .url("https://tupizzeria.com/license")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8081").description("Servidor Local"),
+                        new Server().url("https://api.tupizzeria.com").description("Servidor de Producción")
+                ));
+    }
+}
