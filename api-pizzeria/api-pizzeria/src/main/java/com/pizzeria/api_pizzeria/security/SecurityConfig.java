ee0f461027512424ed5e09012a5688aad84ff3e3
JohanCotrina <johancotrina1233@gmail.com>
2026-06-24 09:51:52 -0500

    feat: configuración de CORS para permitir peticiones desde Vercel"

--- api-pizzeria/api-pizzeria/src/main/java/com/pizzeria/api_pizzeria/security/SecurityConfig.java
+++ api-pizzeria/api-pizzeria/src/main/java/com/pizzeria/api_pizzeria/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pizzeria.api_pizzeria.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.cors(Customizer.withDefaults());
 
         http.csrf(csrf -> csrf.disable());
 
